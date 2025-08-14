5de3ce2fcf900c57503daf2b1a79186de4d35edb
Ikejiri-1 <pedroike21@gmail.com>
2025-08-14 14:59:01 -0300

    implementação do back-end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
-import { FormularioContatoComponent } from './paginas/formulario-contato/formulario-contato.component';
-import { ListaContatosComponent } from './paginas/lista-contatos/lista-contatos.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
