b0c406ea4b996038719d76a1f4281cb6a91a2ec6
Ikejiri-1 <pedroike21@gmail.com>
2025-08-16 12:33:11 -0300

    método activatedRoute

--- src/app/paginas/perfil-contato/perfil-contato.component.ts
+++ src/app/paginas/perfil-contato/perfil-contato.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ContainerComponent } from '../../componentes/container/container.component';
 import { Contato } from '../../componentes/contato';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-perfil-contato',
   standalone: true,
-  imports: [ContainerComponent, RouterLink],
+  imports: [ContainerComponent, RouterLink, CommonModule],
   templateUrl: './perfil-contato.component.html',
   styleUrl: './perfil-contato.component.css',
 })
-export class PerfilContatoComponent {
+export class PerfilContatoComponent implements OnInit {
+  constructor(private activatedRoute: ActivatedRoute) {}
+  ngOnInit() {
+    this.activatedRoute.snapshot.paramMap.get('id');
+  }
+
   contato: Contato = {
     id: 0,
     nome: 'dev',
